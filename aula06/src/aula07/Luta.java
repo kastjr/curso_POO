e7cc4ea6d927f65c45ebe1ddaffa68c68933bf1e
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-10-31 09:22:19 -0300

    Update Luta.java

--- aula06/src/aula07/Luta.java
+++ aula06/src/aula07/Luta.java
@@ -1,1 +1,69 @@
-aaaa
+package UltraEmojiCombat;
+
+public class Luta {
+	//Atributos
+	
+	private Lutador desafiado;
+	private Lutador desafiante;
+	private int rounds;
+	private boolean aprovada;
+	
+	//Metodos
+	
+	public void marcarLuta(Lutador l1,Lutador l2) {
+		if ((l1.getCategoria() == l2.getCategoria()) &&l1 !=l2) {
+			
+			this.aprovada  = true;
+			this.desafiado =  l1;
+			this.desafiante = l2;
+		} else {
+			this.aprovada = false;
+			this.desafiado = null;
+			this.desafiante = null;
+		}
+	}
+	
+	public void lutar() {
+		
+	}
+
+	//Metodos especiais
+	
+	public Lutador getDesafiado() {
+		return desafiado;
+	}
+
+	public void setDesafiado(Lutador desafiado) {
+		this.desafiado = desafiado;
+	}
+
+	public Lutador getDesafiante() {
+		return desafiante;
+	}
+
+	public void setDesafiante(Lutador desafiante) {
+		this.desafiante = desafiante;
+	}
+
+	public int getRounds() {
+		return rounds;
+	}
+
+	public void setRounds(int rounds) {
+		this.rounds = rounds;
+	}
+
+	public boolean isAprovada() {
+		return aprovada;
+	}
+
+	public void setAprovada(boolean aprovada) {
+		this.aprovada = aprovada;
+	}
+	
+	
+	
+	
+	
+}
+
