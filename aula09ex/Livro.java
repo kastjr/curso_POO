84fe831d2cec0a8351f2ea83bd4a9669a3db1198
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-11-04 10:44:31 -0300

    Add files via upload

---
+++ aula09ex/Livro.java
@@ -0,0 +1,69 @@
+package aula09ex;
+
+public class Livro {
+	
+	private String titulo;
+	private String autor;
+	private int totPaginas;
+	private int pagAtual;
+	private boolean aberto;
+	private String leitor;
+	
+	//metodoss espeeciais 
+	
+	public void detalhes() {
+		
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getTotPaginas() {
+		return totPaginas;
+	}
+
+	public void setTotPaginas(int totPaginas) {
+		this.totPaginas = totPaginas;
+	}
+
+	public int getPagAtual() {
+		return pagAtual;
+	}
+
+	public void setPagAtual(int pagAtual) {
+		this.pagAtual = pagAtual;
+	}
+
+	public boolean isAberto() {
+		return aberto;
+	}
+
+	public void setAberto(boolean aberto) {
+		this.aberto = aberto;
+	}
+
+	public String getLeitor() {
+		return leitor;
+	}
+
+	public void setLeitor(String leitor) {
+		this.leitor = leitor;
+	}
+	
+	
+	 
+	
+}
