de478fc6c58447b8aff9f7e191a115f0aad5abf5
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-11-14 15:53:00 -0300

    Update projetopessoa.java

--- projetopessoa/projetopessoa.java
+++ projetopessoa/projetopessoa.java
@@ -2,6 +2,19 @@ package projetopessoa;
 public class ProjetoPessoa {
     public static void main(String[] args) {
         
-        
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+
+        p1.setNome("pedro");
+        p2.setNome("maria");
+        p3.setNome("claudio");
+        p4.setNome("fabiana");
+
+        p2.setCurso("informatica");
+        p3.setSalario(2500.00)
+        p4.setSetor("estoque")
+
     }
 }
