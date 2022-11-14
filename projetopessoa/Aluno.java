2c473d19b88a4a3052dc8a7f52c962446ea61e01
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-11-14 15:14:17 -0300

    Add files via upload

---
+++ projetopessoa/Aluno.java
@@ -0,0 +1,9 @@
+package projetopessoa;
+public class Aluno{
+    private int mtr;
+    private String curso;
+
+    public void cancelarMatricula() {
+        System.out.println("Matricula será cancelada.")
+    }
+}
---
+++ projetopessoa/projetopessoa.java
@@ -0,0 +1,7 @@
+package projetopessoa;
+public class ProjetoPessoa {
+    public static void main(String[] args) {
+        
+        
+    }
+}
