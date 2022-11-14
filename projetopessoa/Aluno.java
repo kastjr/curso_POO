ff9e4928643b5ce78a04f74ddc83b26c2e05234a
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-11-14 15:51:14 -0300

    Update Aluno.java

--- projetopessoa/Aluno.java
+++ projetopessoa/Aluno.java
@@ -1,9 +1,26 @@
 package projetopessoa;
-public class Aluno{
+public class Aluno extends Pessoa {
     private int mtr;
     private String curso;
 
     public void cancelarMatricula() {
         System.out.println("Matricula será cancelada.")
+
+    }
+
+    public int getMtr() {
+        return mtr;
+    }
+
+    public void setMtr(int mtr) {
+        this.mtr = mtr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
     }
 }
