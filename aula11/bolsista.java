7001fdd441e704dfd47bed88dd0686fb518fc770
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-11-15 12:34:51 -0300

    Add files via upload

---
+++ aula11/bolsista.java
@@ -0,0 +1,22 @@
+package aula11;
+public class bolsista extends Aluno {
+    private float bolsa;
+
+    public void renovarBolsa() {
+
+    }
+
+    @Overrride
+    public void pagarMensalidade() {
+
+    }
+
+    public float getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
+
+}
