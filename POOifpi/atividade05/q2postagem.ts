dd33a90ad2ecd0c8096f57279ff760ecc95305b2
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-11-19 18:21:35 -0300

    Add files via upload

---
+++ POOifpi/atividade05/q2postagem.ts
@@ -0,0 +1,74 @@
+class Postagem {
+    id: number
+    texto: string
+    curtidas: number
+
+    constructor(id: number, texto: string, qtd_curtidas: number) {
+        this.id = id
+        this.texto = texto
+        this.curtidas = qtd_curtidas
+    }
+
+    curtir(): void {
+        this.curtidas++
+    }
+
+    toString(): string {
+        return `Texto: ${this.texto}\nCurtidas: ${this.curtidas}`
+    }
+}
+
+class Microblog {
+    postagens: Postagem[] = []
+
+    incluirPostagem(post: Postagem): void {
+        this.postagens.push(post)
+    }
+
+    excluirPostagem(id: number): void {
+        let postAux: Postagem;
+
+        for (let i: number = 0; i < this.postagens.length; i++) {
+            if (this.postagens[i].id == id) {
+                for (let j: number = i; j < this.postagens.length; j++) {
+                    postAux = this.postagens[j + 1] // guarda o post seguinte em memória
+                    this.postagens[j] = this.postagens[j + 1] // move o post atual pra frente
+                    this.postagens[j] = postAux // restaura post guardado
+                }
+
+                this.postagens.pop()
+            }
+        }
+    }
+
+    postagemMaisCurtida(): string {
+        let postagem: Postagem = this.postagens[0];
+
+        for (let i: number = 1; i < this.postagens.length; i++) {
+            if (this.postagens[i].curtidas > postagem.curtidas) {
+                postagem = this.postagens[i]
+            }
+        }
+
+        return postagem.toString()
+    }
+}
+
+
+let blog: Microblog = new Microblog()
+
+const post1: Postagem = new Postagem(1, 'comeca a copa', 3000)
+const post2: Postagem = new Postagem(2, 'Brasil na final', 5000)
+const post3: Postagem = new Postagem(3, 'Brasil campeao', 10000)
+
+blog.incluirPostagem(post1)
+blog.incluirPostagem(post2)
+blog.incluirPostagem(post3)
+
+post1.curtir();
+post2.toString();
+
+blog.excluirPostagem(2)
+console.log(blog.postagens);
+
+console.log(blog.postagemMaisCurtida())
