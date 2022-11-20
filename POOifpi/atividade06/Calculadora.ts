6d98ea70f06da7ce7d8856218c0940cb36a71513
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-11-20 18:03:48 -0300

    Update Calculadora.ts

--- POOifpi/atividade06/Calculadora.ts
+++ POOifpi/atividade06/Calculadora.ts
@@ -21,4 +21,6 @@ let calc: Calculadora = new Calculadora(100, 200);
 console.log(`Soma: ${calc.soma()}`)
 console.log(`Subtração: ${calc.subtrai()}`)
 
-// console.log(`_operando1: ${calc._operando1}`) erro (atributo privado)
+// console.log(`operando1: ${calc._operando1}`) erro (atributo privado)
+// console.log(`operando2: ${calc._operando2}`) erro (atributo privado)
+// Ambos atributos sao privados por isso no codigo principal , em forma de encapsular ou proteger o codigo, não é possivel acessar esses atributos.
