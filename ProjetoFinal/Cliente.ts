5552000f461c2dddf52a2101b58295823c4fdfae
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-12-04 14:37:22 -0300

    Add files via upload

---
+++ ProjetoFinal/Cliente.ts
@@ -0,0 +1,59 @@
+
+class Padrao {
+    //private listaTimes : new Array[];
+    private id : number;
+    private usuario : string ;
+    private senha : string ; 
+
+    constructor(id : number ,usuario:string , senha :string ) {
+        this.id = id;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    // public valida() boolean {
+
+   // }
+
+    public criarTime(): void {
+
+    }
+
+    public verMeusTimes(): void{
+
+    }
+
+    public verElenco (): void {
+
+    }
+
+    public deletarTime():void {
+
+    }
+}
+
+class Admin {
+    private id : number;
+    private usuario : string ;
+    private senha : string ;
+
+    constructor(id : number ,usuario:string , senha :string ) {
+        this.id = id;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public criarJogadores() : void {
+
+    }
+
+    public alterarOverall(): void {
+
+    }
+
+    public deletarJogadores() : void {
+        
+    }
+
+    
+}
