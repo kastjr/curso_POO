ad9f3775a1ed418b4af9cd27b116eea9e9dd004a
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-12-11 11:16:51 -0300

    Update Controle.ts

--- ProjetoFinal/Controle.ts
+++ ProjetoFinal/Controle.ts
@@ -1,2 +1,18 @@
 Interface Controle {
+
+    public criarTime(): void {
+
+    }
+
+    public verMeusTimes(): void{
+
+    }
+
+    public verElenco (): void {
+
+    }
+
+    public deletarTime():void {
+
+    }
 }
